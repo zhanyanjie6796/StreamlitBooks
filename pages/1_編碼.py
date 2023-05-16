335616d669cd3e35a1403a916721ed933f9c117c
zhanyanjie6796 <jack6796@gmail.com>
2023-05-16 16:20:16 +0800

    first commit

---
+++ pages/1_編碼.py
@@ -0,0 +1,19 @@
+import streamlit as st
+# import tempfile
+from PyPDF2 import PdfReader
+
+def style_func():
+    st.write('按鈕處理')
+
+def main():
+    col1, col2 = st.columns([0.6, 0.4])  # Adjust column width
+    st.title("建置中....")    
+
+    if st.button('測試按鈕'):
+        style_func()
+
+    # for i in range(0,100,10):
+    #     st.progress(i + 1)
+    
+if __name__ == "__main__":
+    main()
---
+++ pages/4_try上傳多圖片.py
@@ -0,0 +1,33 @@
+import streamlit as st
+# import tempfile
+from PyPDF2 import PdfReader
+
+def style_func():
+    st.write('風格化處理')
+
+def main():
+    # col1, col2 = st.columns([0.6, 0.4])  # Adjust column width
+    st.title("上傳與瀏覽程式")    
+    style_file = st.file_uploader("請上傳多圖片",accept_multiple_files=True)
+
+    # if style_file:
+    #     stringio = style_file.getvalue()
+    #     style_file_path = 'style_file/'+ style_file.name
+    #     with open(style_file_path,'wb') as f:
+    #         f.write(stringio)
+
+    # style_file_path = 'style_file/'+ style_file.name
+    if style_file:
+        style_file_path = style_file[0].getvalue()
+        st.image(style_file_path)        
+
+    st.write('高解析度風格化demo')
+
+    if st.button('開始進行風格化處理'):
+        style_func()
+
+    # for i in range(0,100,10):
+    #     st.progress(i + 1)
+    
+if __name__ == "__main__":
+    main()
