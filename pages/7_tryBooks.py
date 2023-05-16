076880cbd359e47a59bb640705c2ca2cc426ed0d
zhanyanjie6796 <jack6796@gmail.com>
2023-05-16 17:22:25 +0800

    update

--- pages/7_tryBooks.py
+++ pages/7_tryBooks.py
@@ -1,19 +1,14 @@
 import streamlit as st
-# import tempfile
-# from PyPDF2 import PdfReader
-
-def style_func():
-    st.write('風格化處理')
+import os
 
 def main():
-
-
-    st.write('高解析度風格化demo')
-
-    if st.button('開始進行風格化處理'):
-        style_func()
-
-
     
+    os.environ["OPENAI_API_KEY"] = os.getenv("AZURE_OPENAI_API_KEY") #修改成自己的 API_KEY。例如："39............................bb"
+
+
+    st.write("高解析度風格化demo"+os.environ["OPENAI_API_KEY"])
+
+
+   
 if __name__ == "__main__":
     main()
