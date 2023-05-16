df8bf9d7e37fd50a9895653585aca576c93b916d
zhanyanjie6796 <jack6796@gmail.com>
2023-05-16 17:25:19 +0800

    update

--- pages/7_tryBooks.py
+++ pages/7_tryBooks.py
@@ -1,12 +1,10 @@
 import streamlit as st
-import os
+
 
 def main():
-    
-    os.environ["OPENAI_API_KEY"] = os.getenv("AZURE_OPENAI_API_KEY") #修改成自己的 API_KEY。例如："39............................bb"
-
-
-    st.write("高解析度風格化demo"+os.environ["OPENAI_API_KEY"])
+    import os
+    os.environ["OPENAI_API_KEY"] = os.getenv("AZURE_OPENAI_API_KEY") #修改成自己的 API_KEY。例如："39............................bb"                
+    st.write("高解析度風格化demo")
 
 
    
