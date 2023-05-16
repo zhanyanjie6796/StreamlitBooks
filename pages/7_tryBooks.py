df99dc8b08320a99f55ac6ac0b96704fb408c524
zhanyanjie6796 <jack6796@gmail.com>
2023-05-16 17:39:08 +0800

    update

--- pages/7_tryBooks.py
+++ pages/7_tryBooks.py
@@ -8,6 +8,6 @@ def main():
    
 if __name__ == "__main__":
     
-    os.environ["OPENAI_API_KEY"] = os.getenv("AZURE_OPENAI_API_KEY") #修改成自己的 API_KEY。例如："39............................bb"                
-    st.write("高解析度風格化demo"+os.environ["OPENAI_API_KEY"])    
+    # os.environ["OPENAI_API_KEY"] = os.getenv("AZURE_OPENAI_API_KEY") #修改成自己的 API_KEY。例如："39............................bb"                
+    st.write("高解析度風格化demo"+os.getenv("AZURE_OPENAI_API_KEY"))    
     main()
