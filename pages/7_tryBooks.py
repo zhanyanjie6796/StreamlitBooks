fc75eac17efa6868e3111b499e8d1d1bbf89462c
zhanyanjie6796 <jack6796@gmail.com>
2023-05-17 17:21:27 +0800

    update

--- pages/7_tryBooks.py
+++ pages/7_tryBooks.py
@@ -13,7 +13,7 @@ def main():
     os.environ["OPENAI_API_KEY"] = os.getenv("AZURE_OPENAI_API_KEY") # 修改成自己的 API_KEY。例如："39............................bb"
     st.write("demo："+os.environ["OPENAI_API_KEY"]) # 設定在 streamlit 網站的 Secrets 的環境變數中。
 
-    pdf_path =  "docs1_AI1"
+    pdf_path =  "docs1_AI"
     # pdf_path =  "docs2_Buddhism"
     # pdf_path =  "docs3_merge_AI_Buddhism"
     data_store = pdf_path + "/data_store" 
@@ -35,7 +35,27 @@ def main():
         # print(f"Missing files. Upload index.faiss and index.pkl files to data_store directory first")
         st.write("Missing files. Upload index.faiss and index.pkl files to data_store directory first")
     
-    st.write("vector_store")
+    # Query using the vector store
+    # Set up the chat model and specific prompt
+
+    from langchain.prompts.chat import (
+        ChatPromptTemplate,
+        SystemMessagePromptTemplate,
+        HumanMessagePromptTemplate,
+    )
+
+    system_template="""Use the following pieces of context to answer the users question.
+    Take note of the sources and include them in the answer in the format: "SOURCES: source1 source2", use "SOURCES" in capital letters regardless of the number of sources.
+    If you don't know the answer, just say that "I don't know", don't try to make up an answer.
+    ----------------
+    {summaries}"""
+    messages = [
+        SystemMessagePromptTemplate.from_template(system_template),
+        HumanMessagePromptTemplate.from_template("{question}")
+    ]
+    prompt = ChatPromptTemplate.from_messages(messages)    
+
+    st.write("哈哈哈")
    
 if __name__ == "__main__":   
     main()
