a635f26debf5ac729fa8a4321c24b528fe559e35
zhanyanjie6796 <jack6796@gmail.com>
2023-05-18 14:48:14 +0800

    update

--- pages/2_Query查詢.py
+++ pages/2_Query查詢.py
@@ -126,7 +126,7 @@ def main():
     for index, document in enumerate(source_documents):
         # txt = st.text_area("文獻來源：", "哈哈哈\n你好")         
         st.text_area("Source"+str(index + 1)+"&nbsp;&nbsp;&nbsp;&nbsp;檔名："+document.metadata['source']+"&nbsp;&nbsp;&nbsp;&nbsp;頁碼："+str(document.metadata['page']+1), 
-                     document.page_content,height=300)
+                     document.page_content,height=200)
         # 下面是原本的程式
         # st.write(f"\n\nSource {index + 1}:")
         # st.write("檔名："+document.metadata['source']+"    頁碼："+str(document.metadata['page']+1))    
