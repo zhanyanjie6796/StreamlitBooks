1a1c2f4694e6e54845f52063e3232efbe71d2493
zhanyanjie6796 <jack6796@gmail.com>
2023-05-18 15:27:57 +0800

    update

--- pages/2_Query詢問.py
+++ pages/2_Query詢問.py
@@ -16,7 +16,7 @@ def main():
         st.markdown("#### Query 詢問")
     
     st.write('這裏的查詢資料以 docs1_AI 中的 PDF 檔案之向量索引爲例。')
-    title  = st.text_input("請輸入您要詢問的問題，例如：人工智慧的應用場景分成四大面向,台灣式繁體中文回答。")
+    title  = st.text_input("###### 請輸入您要詢問的問題，例如：人工智慧的應用場景分成四大面向,台灣式繁體中文回答。")
 
     if title == "":
         import sys # st.write('== 中斷測試 ==')
