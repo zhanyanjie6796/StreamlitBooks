d28c58d79220cb26f6abec9ff227b79a5a24f8d0
zhanyanjie6796 <jack6796@gmail.com>
2023-05-18 10:52:18 +0800

    update

--- home.py
+++ home.py
@@ -4,17 +4,17 @@ st.markdown('# 歡迎來到')
 st.markdown('# Azure openAI langchain PDF')
 st.markdown('----')
 st.markdown('## 這裏可以演示 langchain PDF 的編碼和查詢')
-# st.write('這裏可以演示 langchain PDF 的編碼和查詢')
-
+# st
 
 # AZURE_OPENAI_API_KEY  
 if 'AZURE_OPENAI_API_KEY' not in st.session_state or st.session_state['AZURE_OPENAI_API_KEY'] == "": 
     inputkey = st.text_input("請輸入您的 AZURE OPENAI_API_KEY：例如xx39d931157d574944954f02a48c6567bbxx")           
     st.session_state['AZURE_OPENAI_API_KEY'] = inputkey
-else:
+
+if st.session_state['AZURE_OPENAI_API_KEY'] != "":
     st.write('您的 AZURE OPENAI_API_KEY：', st.session_state['AZURE_OPENAI_API_KEY']) 
     st.write('如果要重新輸入，請按 F5 重新整理網頁。')
-    
+
 # if st.session_state['AZURE_OPENAI_API_KEY'] != "":
 #     st.write('您的 AZURE OPENAI_API_KEY：', st.session_state['AZURE_OPENAI_API_KEY']) 
 
