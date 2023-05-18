90f9da85c5280b0d799cd9ba6335d74e10b3c39c
zhanyanjie6796 <jack6796@gmail.com>
2023-05-18 15:04:03 +0800

    update

--- pages/2_Query查詢.py
+++ pages/2_Query查詢.py
@@ -125,7 +125,7 @@ def main():
     source_documents = result['source_documents']
     for index, document in enumerate(source_documents):
         # txt = st.text_area("文獻來源：", "哈哈哈\n你好")         
-        st.text_area("#### Source"+str(index + 1)+"&nbsp;&nbsp;&nbsp;&nbsp;檔名："+
+        st.text_area("Source"+str(index + 1)+"&nbsp;&nbsp;&nbsp;&nbsp;檔名："+
                      document.metadata['source']+"&nbsp;&nbsp;&nbsp;&nbsp;頁碼："+
                      str(document.metadata['page']+1), 
                      document.page_content,height=200)
