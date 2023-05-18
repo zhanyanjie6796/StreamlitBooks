f1df683ecea6af71b0d9fdf86fc19c75223aa674
zhanyanjie6796 <jack6796@gmail.com>
2023-05-18 10:48:59 +0800

    update

--- home.py
+++ home.py
@@ -11,9 +11,12 @@ st.markdown('## 這裏可以演示 langchain PDF 的編碼和查詢')
 if 'AZURE_OPENAI_API_KEY' not in st.session_state or st.session_state['AZURE_OPENAI_API_KEY'] == "": 
     inputkey = st.text_input("請輸入您的 AZURE OPENAI_API_KEY：例如xx39d931157d574944954f02a48c6567bbxx")           
     st.session_state['AZURE_OPENAI_API_KEY'] = inputkey
-
-if st.session_state['AZURE_OPENAI_API_KEY'] != "":
+else:
     st.write('您的 AZURE OPENAI_API_KEY：', st.session_state['AZURE_OPENAI_API_KEY']) 
+    st.write('如果要重新輸入，請按 F5 重新整理網頁。')
+    
+# if st.session_state['AZURE_OPENAI_API_KEY'] != "":
+#     st.write('您的 AZURE OPENAI_API_KEY：', st.session_state['AZURE_OPENAI_API_KEY']) 
 
 # if 'AZURE_OPENAI_API_KEY' in st.session_state:
 #     # st.session_state['key'] = 'value'
--- pages/2_查詢Query.py
+++ pages/2_查詢Query.py
@@ -7,20 +7,25 @@ def style_func():
 
 def main():
     # AZURE_OPENAI_API_KEY   
-    # inputkey = st.text_input("請輸入您的 AZURE OPENAI_API_KEY：例如xx39d931157d574944954f02a48c6567bb")  
-    # if 'AZURE_OPENAI_API_KEY' not in st.session_state:              
-    #     st.session_state['AZURE_OPENAI_API_KEY'] = inputkey
-    if 'AZURE_OPENAI_API_KEY' not in st.session_state:
-        st.write('請回到首頁 home 輸入您的 AZURE OPENAI_API_KEY 再回來。')
-        import sys
-        sys.exit('== 中斷測試 ==')        
 
-    if st.session_state['AZURE_OPENAI_API_KEY'] != "":
-        st.write('您的 AZURE OPENAI_API_KEY：', st.session_state['AZURE_OPENAI_API_KEY'])    
-    else:
+    if 'AZURE_OPENAI_API_KEY' not in st.session_state or st.session_state['AZURE_OPENAI_API_KEY'] == "":
         st.write('請回到首頁 home 輸入您的 AZURE OPENAI_API_KEY 再回來。')
         import sys
-        sys.exit('== 中斷測試 ==')
+        sys.exit('== 中斷測試 ==') 
+    else:
+        st.write('您的 AZURE OPENAI_API_KEY：', st.session_state['AZURE_OPENAI_API_KEY'])  
+
+    # if 'AZURE_OPENAI_API_KEY' not in st.session_state:
+    #     st.write('請回到首頁 home 輸入您的 AZURE OPENAI_API_KEY 再回來。')
+    #     import sys
+    #     sys.exit('== 中斷測試 ==')        
+
+    # if st.session_state['AZURE_OPENAI_API_KEY'] != "":
+    #     st.write('您的 AZURE OPENAI_API_KEY：', st.session_state['AZURE_OPENAI_API_KEY'])    
+    # else:
+    #     st.write('請回到首頁 home 輸入您的 AZURE OPENAI_API_KEY 再回來。')
+    #     import sys
+    #     sys.exit('== 中斷測試 ==')
 
     st.write('對話方塊測試')
     title  = st.text_input("請輸入您要詢問的問題：")
