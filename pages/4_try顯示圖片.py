588092d5efc717585fb388d91e939649d3324ef5
zhanyanjie6796 <jack6796@gmail.com>
2023-05-18 14:51:53 +0800

    update

--- pages/4_try顯示圖片.py
+++ pages/4_try顯示圖片.py
@@ -26,6 +26,14 @@ def main():
     if st.button('開始進行風格化處理'):
         style_func()
 
+    txt = st.text_area('文獻來源：', '''
+    It was the best of times, it was the worst of times, it was
+    the age of wisdom, it was the age of foolishness, it was
+    the epoch of belief, it was the epoch of incredulity, it
+    was the season of Light, it was the season of Darkness, it
+    was the spring of hope, it was the winter of despair, (...)
+    ''')
+    st.write('Sentiment:', (txt)) 
 
     
 if __name__ == "__main__":
