3dc7aeaa773fec3f1d1759c9270889533f273061
zhanyanjie6796 <jack6796@gmail.com>
2023-05-22 09:13:25 +0800

    update

--- pages/2_Query詢問.py
+++ pages/2_Query詢問.py
@@ -15,7 +15,7 @@ def main():
         # st.write('您的 AZURE OPENAI_API_KEY：', st.session_state['AZURE_OPENAI_API_KEY'])  
         st.markdown("## Query 詢問")
     
-    st.write('這裏的查詢資料以 docs1_AI 中的 PDF 檔案之向量索引爲例。')
+    st.write('這裏的查詢資料以 docs3_merge_AI_Buddhism 中的 PDF 檔案之向量索引爲例。')
     title  = st.text_input("###### 請輸入您要詢問的問題，例如：人工智慧的應用場景分成四大面向,台灣式繁體中文回答。")
 
     if title == "":
